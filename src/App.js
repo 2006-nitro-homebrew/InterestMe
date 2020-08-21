136545fd745207862f37250dddc7e756738f42a8
Jake <jakejaekwak@gmail.com>
2020-08-21 16:17:09 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; // add
+
 import Routes from "./routes";
 
 function App() {
   return (
+    <MuiThemeProvider>
+       <Navbar />
     <div className="App">
       <header className="App-header">
         {/* Edit <code>src/App.js</code> and save to reload. */}
         <p> HELLO WORLD!</p>
-        {/* <Navbar /> */}
+
         <Routes />
       </header>
     </div>
+    </MuiThemeProvider>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core'
+import {Menu} from '@material-ui/icons'
+
+function Navbar() {
+  return (
+    <div>
+      <AppBar position = 'sticky'>
+        <Toolbar>
+          <IconButton color='inherit' edge='start' aria-label='menu'>
+
+          <Button color='inherit' >
+            Home
+          </Button>
+          </IconButton>
+          <div className='navButton'>
+          <Button  color='inherit'>
+            Reading List
+          </Button>
+          <Button color='inherit'>
+            Interest Me
+          </Button>
+          <Button  color='inherit'>
+            Profile
+          </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default Navbar
