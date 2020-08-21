1bbb04e7b2450f2978cfd967e53184f9eb6c434e
Jake <jakejaekwak@gmail.com>
2020-08-21 17:20:36 -0400

    additional navbar changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core'
 import {Menu} from '@material-ui/icons'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
@@ -13,18 +14,26 @@ function Navbar() {
           </Button>
           </IconButton>
           <div className='navButton'>
+            <Link to={'/readinglist'} style={{textDecoration: 'none', color: '#FFF'}}>
           <Button  color='inherit'>
             Reading List
           </Button>
+          </Link>
+          <Link to={'/interestMe'} style={{textDecoration: 'none', color: '#FFF'}}>
           <Button color='inherit'>
             Interest Me
           </Button>
+          </Link>
+          <Link to={'/profile'} style={{textDecoration: 'none', color: '#FFF'}}>
           <Button  color='inherit'>
             Profile
           </Button>
+          </Link>
+          <Link to={'/login'} style={{textDecoration: 'none', color: '#FFF'}}>
           <Button  color='inherit'>
             Login
           </Button>
+          </Link>
           </div>
         </Toolbar>
       </AppBar>
