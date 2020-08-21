34887c91791033a51297bb584365fab28e1df3e1
Claudia <claudia.sinowato@gmail.com>
2020-08-21 12:52:33 -0400

    Fixed boilerplate react bug

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { withRouter, Route, Switch } from "react-router-dom";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { Login, Signup, InterestMe } from "./components";
-// import Signup from "./components/Login";
 
-export default class Routes extends Component {
+export default class Routes extends React.Component {
   render() {
-    const { isLoggedIn } = this.props;
+    // const { isLoggedIn } = this.props;
+    const isLoggedIn = true; // TESTING
+
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
+        {/* <Route path="/signup" component={Signup} /> */}
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={InterestMe} />
+            {/* <Route path="/home" component={InterestMe} /> */}
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
