f93212a8300325b2c580554235cad775fcc0e559
Claudia <claudia.sinowato@gmail.com>
2020-08-23 18:31:23 -0400

    FE: Redux for getting all articles

--- src/components/ReadingList.js
+++ src/components/ReadingList.js
@@ -1,51 +1,77 @@
 import React from "react";
 import db from "../db/index";
+import { connect } from "react-redux";
+import { fetchArticles } from "../store/articles";
 
-export default class ReadingList extends React.Component {
+export class ReadingList extends React.Component {
   constructor() {
     super();
-    this.state = {
-      list: {},
-    };
+    // this.state = {
+    //   list: {},
+    // };
   }
 
   async componentDidMount() {
-    db.collection("users/testuser/savedOffline")
-      .onSnapshot((snapshot) => {
-        const data = snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        this.setState({ list: data });
-      });
+    this.props.getArticles();
+    // db.collection("users/testuser/savedOffline").onSnapshot((snapshot) => {
+    //   const data = snapshot.docs.map((doc) => ({
+    //     id: doc.id,
+    //     ...doc.data(),
+    //   }));
+    //   this.setState({ list: data });
+    // });
   }
 
   renderTableHeader() {
-    let header = Object.keys(this.state.list[0]).filter(key => key != "content")
+    let header = Object.keys(this.props.list[0]).filter(
+      (key) => key != "content"
+    );
     return header.map((key, index) => {
-      return <th key={index}>{key.toUpperCase()}</th>
-    })
+      return <th key={index}>{key.toUpperCase()}</th>;
+    });
   }
 
   render() {
-    let allList = this.state.list
-    console.log(allList)
+    let allList = this.props.list;
+    console.log(allList);
     return (
       <div>
-        <h3 id='title'>Saved Reading List</h3>
-        <table id='readinglist'>
-        <tbody>
-          {(allList.length > 0)?<tr>{this.renderTableHeader()}</tr>:console.log()}
-          {(allList.length > 0) ? allList.map(doc => (
-            <tr key={doc.id}>
-              <td>{doc.id}</td>
-              <td>{doc.name}</td>
-              <td>{doc.source}</td>
-            </tr>
-          )) : <h1>Nothing Saved</h1>}
-        </tbody>
+        <h3 id="title">Saved Reading List</h3>
+        <table id="readinglist">
+          <tbody>
+            {allList.length > 0 ? (
+              <tr>{this.renderTableHeader()}</tr>
+            ) : (
+              console.log()
+            )}
+            {allList.length > 0 ? (
+              allList.map((doc) => (
+                <tr key={doc.id}>
+                  <td>{doc.id}</td>
+                  <td>{doc.name}</td>
+                  <td>{doc.source}</td>
+                </tr>
+              ))
+            ) : (
+              <h1>Nothing Saved</h1>
+            )}
+          </tbody>
         </table>
-      </div >
+      </div>
     );
   }
 }
+
+const mapState = (state) => {
+  return {
+    list: state.articles,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    getArticles: () => dispatch(fetchArticles()),
+  };
+};
+
+export default connect(mapState, mapDispatch)(ReadingList);
